e195ed3b217ada6b401066ba5dd69de885730105
szbence26 <szokebenc@gmail.com>
2025-12-22 13:25:45 +0100

    update build

---
+++ assets/use-dark.4ee42811.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.60cc5fab.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
--- src/resources/colors.js
+++
@@ -1,5 +0,0 @@
-export default {
-  main1: "#0f5216",
-  main2: "#259c33",
-  bgColor: "#8fbf95",
-};
